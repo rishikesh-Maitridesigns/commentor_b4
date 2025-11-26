e35d113d7be0e2c05889852617d216879a033816
rishikesh-Maitridesigns <rishikesh@maitridesigns.com>
2025-11-26 19:23:22 +0530

    Updated supabase.config.js

--- extension/supabase.config.js
+++ extension/supabase.config.js
@@ -1,6 +1,6 @@
 const SUPABASE_CONFIG = {
-  url: 'https://kfhemlqgwfkbqpoqsjgn.supabase.co',
-  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtmaGVtbHFnd2ZrYnFwb3FzamduIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM3MjY1ODUsImV4cCI6MjA3OTMwMjU4NX0.TGXLn91XAHMtCwAaXjWi3E4Z79OxJnJRZPgGV2SYOhw'
+  url: 'https://evpskuhskpmrbbihdihd.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV2cHNrdWhza3BtcmJiaWhkaWhkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM2MTk3MTQsImV4cCI6MjA3OTE5NTcxNH0.1TFjxV7csnWm6cZTFIoreeEctFF799fruxGJByyV1kQ'
 };
 
 const getSupabaseUrl = () => SUPABASE_CONFIG.url;
